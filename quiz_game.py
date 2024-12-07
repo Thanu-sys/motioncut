d4ed90b93cfb6867a7d8ae0f9227357982c43b34
Thanu-sys <thanusreevuppu025@gmail.com>
2024-12-08 01:30:15 +0530

    hi

---
+++ quiz_game.py
@@ -0,0 +1,11 @@
+print("WELCOME TO FUN QUIZ 🧠")
+print("Our quize game put the fun into learing.Your knowledge will be tested on a variety of  fun and intresting topics.")
+
+playing = input("If you are intrested please press yes to begin: ")
+
+if playing.lower()!="yes":
+  quit()
+print("Okay! lets have fun 😁")
+
+def quiz:
+  print("play")
