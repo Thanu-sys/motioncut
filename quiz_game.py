778a7344e894a8bd5b8a7ff83985d42416f639c7
Thanu-sys <thanusreevuppu025@gmail.com>
2024-12-08 21:31:44 +0530

    started

--- quiz_game.py
+++ quiz_game.py
@@ -7,5 +7,16 @@ if playing.lower()!="yes":
   quit()
 print("Okay! lets have fun 😁")
 
-def quiz:
-  print("play")
+def quiz():
+  score=0
+  ans=print("Which ocean is the deepest on earth?")
+  opt=input("1.Indian Ocean  2.Pacific Ocean  3.Arctic Ocean")
+  if opt=='2':
+    print("Correct✅")
+    score+=1
+  else:
+    print("Incorrect❎\nPacific Ocean✅ ")
+  print(score)
+
+
+quiz()
