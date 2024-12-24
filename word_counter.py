7c3143b1e020947533ea5a8e717f91d2572bdb98
Thanu-sys <thanusreevuppu025@gmail.com>
2024-12-24 12:28:00 +0530

    done

--- word_counter.py
+++ word_counter.py
@@ -1,8 +1,11 @@
-user=input()
-print(user)
-b=user.split()
-print(b)
-count=1
-for i in range(1,len(b)):
-  count+=1
-print(count)
+def wordcount(b):
+  count=1
+  for i in range(1,len(b),1):         #for loop used to count the number of words present in sentence or paragraph.
+    count+=1
+  return count
+
+user=input("Please enter a sentence or a paraghraph: \n")
+b=user.split()                       #split() methond coverts the string into list for esay counting.
+result=wordcount(b)                  # it stores the return value from wordcount function.
+print(b)                             #words for debugging for proper undersanding.
+print("WORD COUNT: ",result) 
